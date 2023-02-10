1f7dfe9e3141eb2bbc84f7b5a84a4bf259305e1b
stmayna <stmayna@gmail.com>
2023-02-10 13:47:54 +0700

    enhancement maintenance_request

--- hit_maintenance/models/maintenance_request.py
+++ hit_maintenance/models/maintenance_request.py
@@ -4,15 +4,15 @@ from odoo import _, api, fields, models
 class MaintenanceRequest(models.Model):
     _inherit = 'maintenance.request'
 
-    account_group = fields.Many2one(
-        comodel_name='account.analytic.group', string='Account Group')
+    # account_group = fields.Many2one(
+    #     comodel_name='account.analytic.group', string='Account Group')
     analytic_code = fields.Char(string='Analytic Code')
-    category = fields.Char(string='Category')
+    category = fields.Char(string='Categories')
     close_date = fields.Date(string='Done Date')
     create_reservation = fields.Boolean(string='Create Reservation')
     expence_element = fields.Char(string='Expence Element')
     # I don't know why the technical name is 'schedulle', I want to change it but it might break the whole system
-    maintenance_type = fields.Selection(string='Maintenance Type', selection=[
-                                        ('Schedulle', 'Schedule'), ('Unschedulle', 'Unschedule')])
+    # maintenance_type = fields.Selection(string='Maintenance Type', selection=[
+    #                                     ('Schedule', 'Schedule'), ('Unschedule', 'Unschedule')])
     analytic_account = fields.Many2one(
         comodel_name='account.analytic.account', string='Analytic Account')

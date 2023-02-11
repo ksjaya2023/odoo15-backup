ba468327e0e4ecedb5aa55482a2b46f651135cb7
stmayna <stmayna@gmail.com>
2023-02-11 14:02:57 +0700

    cannot find the analytic group

--- hit_maintenance/models/standard_job.py
+++ hit_maintenance/models/standard_job.py
@@ -8,8 +8,8 @@ class StandardJob(models.Model):
     name = fields.Char(string='Standard Job')
     active = fields.Boolean(string='Active', default=True)
     sequence = fields.Integer(string='Sequence')
-    account_group = fields.Many2one(
-        comodel_name='account.analytic.group', string='Account Group')  # related
+    # account_group = fields.Many2one(
+    #     comodel_name='account.analytic.group', string='Account Group')  # related
     analytic_account_id = fields.Many2one(
         comodel_name='account.analytic.account', string='Analytic Account')
     notes = fields.Html(string='Notes')

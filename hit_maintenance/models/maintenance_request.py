f1a51bd35612ea24820d5e4d0054f545fb453194
stmayna <stmayna@gmail.com>
2023-02-11 14:04:27 +0700

    create reservation line

--- hit_maintenance/models/maintenance_request.py
+++ hit_maintenance/models/maintenance_request.py
@@ -1,8 +1,16 @@
 from odoo import _, api, fields, models
 
+_STATUS = [
+    ("installed", "Installed"),
+    ("not_installed", "Not Installed"),
+    ("return", "Return"),
+    ("finish", "Finish")
+]
+
 
 class MaintenanceRequest(models.Model):
     _inherit = 'maintenance.request'
+    _description = 'Maintenance Request or Work Order'
 
     # account_group = fields.Many2one(
     #     comodel_name='account.analytic.group', string='Account Group')
@@ -16,3 +24,17 @@ class MaintenanceRequest(models.Model):
     #                                     ('Schedule', 'Schedule'), ('Unschedule', 'Unschedule')])
     analytic_account = fields.Many2one(
         comodel_name='account.analytic.account', string='Analytic Account')
+    maintenance_request_ids = fields.One2many(
+        'maintenance.request.line', 'maintenance_request_id', string='Maintenance Request Ids')
+
+
+class MaintenanceRequestLine(models.Model):
+    _name = 'maintenance.request.line'
+    _description = 'Maintenance Request or Work Order Line'
+
+    name = fields.Char('Description')
+    maintenance_request_id = fields.Many2one(
+        comodel_name='maintenance.request', string='Maintenance Request ID')
+    date = fields.Date('Date')
+    sequence = fields.Integer('Sequence')
+    status = fields.Selection(selection=_STATUS, string='Status')

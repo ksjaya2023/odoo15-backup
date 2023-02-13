c59f506487cce2567789e68a201bf73af987c78e
stmayna <stmayna@gmail.com>
2023-02-13 13:33:26 +0700

    vendor customization

--- hit_purchase/models/purchase_order.py
+++ hit_purchase/models/purchase_order.py
@@ -8,4 +8,13 @@ from odoo.exceptions import UserError, ValidationError
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    
+    complete_gr = fields.Boolean('Complete GR')
+    create_by_pr = fields.Boolean('Create by PR')
+    rfq = fields.Char('RFQ')
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    schedule_date = fields.Date('Schedule Date')
+    status = fields.Char('Purchase Status')

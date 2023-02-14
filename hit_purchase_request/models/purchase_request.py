6777cf11961cbdc540a8641d0c46b4f8bf88fd1b
stmayna <stmayna@gmail.com>
2023-02-14 15:32:34 +0700

    customizaion purchase request

--- hit_purchase_request/models/purchase_request.py
+++ hit_purchase_request/models/purchase_request.py
@@ -10,11 +10,12 @@ class PurchaseRequest(models.Model):
     create_by_reservasi = fields.Boolean('Create by Reservasi')
     create_rfq = fields.Boolean('Create RFQ')
     document_name = fields.Char('Document Name')
-    many2one_field_y7F5a = fields.Many2one(comodel_name='purchase.order', string='Purchase Order')
+    many2one_field_y7F5a = fields.Many2one(
+        comodel_name='purchase.order', string='Purchase Order')
     # many2one_reservation = fields.Many2one(comodel_name='x_reservation', string='Reservation')
     purchase_type = fields.Selection(string='Purchase Type',
-                                          selection=[('Purchase Request', 'Purchase Request'), ('Asset Request', 'Asset Request')])
+                                     selection=[('Purchase Request', 'Purchase Request'), ('Asset Request', 'Asset Request')])
     request_number = fields.Char('Request Number')
-    request_type = fields.Selection(tring='Request Type',
-                                          selection=[('Stock', 'Stock'), ('Non Stock', 'Non Stock'), ('General', 'General')])
+    request_type = fields.Selection(string='Request Type',
+                                    selection=[('Stock', 'Stock'), ('Non Stock', 'Non Stock'), ('General', 'General')])
     rfq = fields.Many2one(comodel_name='purchase.order', string='RFQ')

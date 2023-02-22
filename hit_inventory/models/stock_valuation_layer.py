55250c3d34cac7cb87a5d1fefa18eb86cc542618
stmayna <stmayna@gmail.com>
2023-02-22 07:59:57 +0700

    count the absolute value

--- hit_inventory/models/stock_valuation_layer.py
+++ hit_inventory/models/stock_valuation_layer.py
@@ -17,4 +17,27 @@ class StockValuationLayer(models.Model):
         string='Reservation Description', related='stock_move_id.reservation_description')
     reservation_type = fields.Selection(
         string='Reservation Type', related='stock_move_id.reservation_type')
-    temporary_total_value = fields.Monetary(string='Actual Value')
+    temporary_total_value = fields.Monetary(
+        string='Actual Value', compute='_compute_temporary_total_value')
+
+    @api.depends('value', 'reservation_type', 'operation_type')
+    def _compute_temporary_total_value(self):
+        for record in self:
+            absolute_value = 0
+            actual_cost = 0
+            total_value = record.value
+            if total_value < 0:
+                absolute_value = -1 * total_value
+            else:
+                absolute_value = -1 * total_value
+            absolute_value = absolute_value
+            if record.reservation_type == '2':
+                actual_cost = absolute_value
+            elif record.reservation_type == '1':
+                if record.operation_type.name == 'Inventory Issued':
+                    actual_cost = absolute_value
+                else:
+                    actual_cost = 0
+            actual_cost = actual_cost
+            record.write({'temporary_total_value': actual_cost})
+            record.write({'filter_value': actual_cost})

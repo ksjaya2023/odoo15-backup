c94aa90ccf00c0c9aac87efb0dc28620e29c6cf3
stmayna <stmayna@gmail.com>
2023-02-22 08:00:55 +0700

    equipment count

--- hit_maintenance/models/unit_model.py
+++ hit_maintenance/models/unit_model.py
@@ -9,4 +9,11 @@ class UnitModel(models.Model):
     active = fields.Boolean(string='Active', default=True)
     notes = fields.Html(string='Notes')
     sequence = fields.Integer(string='Sequence')
-    equipment_count = fields.Integer(string='Equipments')  # compute
+    equipment_count = fields.Integer(
+        string='Equipments', compute='_compute_equipment_count')
+
+    @api.depends('name')
+    def _compute_equipment_count(self):
+        for record in self:
+            record.equipment_count = self.env['maintenance.equipment'].search_count(
+                [('unit_model_id', '=', record.id)])

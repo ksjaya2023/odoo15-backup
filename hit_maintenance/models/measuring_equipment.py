9bc6c48291170b1426eea90edc154001370ff5f6
stmayna <stmayna@gmail.com>
2023-02-22 15:07:24 +0700

    compute hourmeter in measuring equipment

--- hit_maintenance/models/measuring_equipment.py
+++ hit_maintenance/models/measuring_equipment.py
@@ -23,7 +23,7 @@ class MeasuringEquipment(models.Model):
     hm_awal = fields.Integer(
         'HM Awal', help='Begin value of Hourmeter.')
     hourmeter = fields.Integer(
-        'Hourmeter Total', help='The Total Computation of Hourmeter.')  # compute
+        'Hourmeter Total', compute='_compute_hourmeter', help='The Total Computation of Hourmeter.')
     measuring_date = fields.Date('Measuring Date')
     measuring_type = fields.Selection(
         selection=_MEASURING_TYPE, string='Measuring Type')
@@ -32,3 +32,18 @@ class MeasuringEquipment(models.Model):
     sisa_serv_type = fields.Char('Sisa Service Type')
     test_hm_awal = fields.Integer('Test HM Awal')
     work_order_id = fields.Many2one('maintenance.request', string='Work Order')
+
+    @api.depends('delta', 'current_hm')
+    def _compute_hourmeter(self):
+        for record in self:
+            total_hm = 0
+            prev_data = record.env['measuring.equipment'].search(
+                ['&', ('equipment_id', '=', record.equipment_id.id), ('name', '!=', record.name)], order='measuring_date desc')
+            if prev_data:
+                for row_prev_data in prev_data:
+                    total_hm = row_prev_data.hourmeter
+                    break
+            if total_hm == 0:
+                # jika tidak ada data sebelumnya maka field HM awal akan di ambil sbg tambahan
+                total_hm = record.hm_awal
+            record.hourmeter = total_hm + record.delta

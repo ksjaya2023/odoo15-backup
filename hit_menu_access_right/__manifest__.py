43fe5a05edd70930b3fa281414573f82188aaee1
probo <sukmohadi.probo@gmail.com>
2023-03-03 09:04:57 +0700

    [INIT] for menu access right

---
+++ hit_menu_access_right/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2018-2019 ForgeFlow, S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    'name': 'HIT Menu Access Right',
+    'author': 'HIT Digital Indonesia',
+    'website': 'http://www.hitconsulting.id',
+    'version': '0.1',
+    'summary': '''
+        Menu Access Right
+    ''',
+    'description': '''
+        Enhancement Accounting Module.
+    ''',
+    'category': 'Accounting',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'depends': ['account'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/hit_account_menu_view.xml',
+        'security/hit_account_security.xml'
+    ],
+    'demo': [],
+}

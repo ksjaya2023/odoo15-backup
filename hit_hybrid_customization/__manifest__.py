70d7a2ea7d100344f15659aadc3aa2d3324bcf64
stmayna <stmayna@gmail.com>
2023-03-09 15:14:25 +0700

    initiate hybrid development

---
+++ hit_hybrid_customization/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HIT Hybrid Customization",
+
+    'summary': """
+        Hybrid Customization""",
+
+    'description': """
+        Enhancement All Module.
+    """,
+
+    'author': "HIT Digital Indonesia",
+    'website': "http://www.hitconsulting.id",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Customization',
+    'version': '0.1',
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+    'depends': ['maintenance'],
+
+    # always loaded
+    # make sure the menu view is on the last
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+}

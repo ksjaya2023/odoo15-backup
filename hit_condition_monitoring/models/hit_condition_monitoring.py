4965ec230f1680322225f25ad39b1f59838c45f5
stmayna <stmayna@gmail.com>
2023-04-07 14:57:22 +0700

    add condition monitoring to menu

--- hit_condition_monitoring/models/hit_condition_monitoring.py
+++ hit_condition_monitoring/models/hit_condition_monitoring.py
@@ -11,12 +11,12 @@ class HitConditionMonitoring(models.Model):
 
     def _get_default_team_id(self):
         MT = self.env['maintenance.team']
-        team = MT.search([('company_id', '=', self.env.company.id)], limit=1)
+        team = MT.search([], limit=1)
         if not team:
             team = MT.search([], limit=1)
         return team.id
 
-    name = fields.Char('Name')
+    name = fields.Char('Name', default='Draft')
     equipment_id = fields.Many2one('maintenance.equipment', string='Equipment')
     work_order_type = fields.Selection(selection=[
         ('Maintenance', 'Service'),
@@ -37,18 +37,29 @@ class HitConditionMonitoring(models.Model):
     location_id = fields.Many2one('stock.warehouse', string='Location')
     warehouse_id = fields.Many2one('stock.location', string='Warehouse')
     maintenance_team_id = fields.Many2one(
-        'maintenance.team', string='Team', required=True, default=_get_default_team_id, check_company=True)
+        'maintenance.team', string='Team', required=True, default=_get_default_team_id)
     responsible_id = fields.Many2one(
-        'res.users', string='Responsible', domain=lambda self: self._get_user_domain())
+        'res.users', string='Responsible')
     schedule_date = fields.Datetime('Schedule Date')
     complete_date = fields.Datetime('Complete Date')
     owner_user_id = fields.Many2one(
-        'res.users', string='Created by User', default=lambda s: s.env.uid)
+        'res.users', string='Created by User', default=lambda s: s.env.uid, readonly=True)
+    hourmeter = fields.Char('Hourmeter')
+    note = fields.Char('Note')
+    attachment = fields.Binary('Upload PDF')
+    attachment_name = fields.Char('Attachment Name')
 
-    @api.model
-    def _get_user_domain(self):
-        domain = []
+    @api.onchange('maintenance_team_id')
+    def _onchange_maintenance_team_id(self):
         if self.maintenance_team_id:
-            domain.append(
-                ('id', 'in', self.maintenance_team_id.member_ids.ids))
-        return domain
+            user_ids = [
+                user.id for user in self.maintenance_team_id.member_ids]
+            domain = [('id', 'in', user_ids)]
+            return {'domain': {'responsible_id': domain}}
+        else:
+            return {'domain': {'responsible_id': []}}
+
+    @api.onchange('attachment')
+    def onchange_attachment(self):
+        if self.attachment:
+            self.attachment_name = 'attachment_' + self.name

1998c3a1e21002d28e426049b437595d150a0198
stmayna <stmayna@gmail.com>
2023-04-11 14:49:05 +0700

    button validate on return to vendor

--- hit_hybrid_customization/models/stock_picking.py
+++ hit_hybrid_customization/models/stock_picking.py
@@ -7,16 +7,9 @@ from odoo.exceptions import UserError, ValidationError
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    show_validate = fields.Boolean(compute='_compute_show_validate')
-
     @api.depends('state', 'x_studio_operation_type_name')
     def _compute_show_validate(self):
         for picking in self:
-            if not (picking.immediate_transfer) and picking.state == 'draft':
-                picking.show_validate = False
-            elif picking.state not in ('draft', 'waiting', 'confirmed', 'assigned') and picking.x_studio_operation_type_name != 'Return to Vendor':
-                picking.show_validate = False
-            elif picking.state in ('draft', 'waiting', 'confirmed', 'assigned') and picking.x_studio_operation_type_name != 'Return to Vendor':
-                picking.show_validate = True
-            elif picking.state != 'assigned' and picking.x_studio_operation_type_name == 'Return to Vendor':
+            super(StockPicking, picking)._compute_show_validate()
+            if picking.state == 'assigned' and picking.x_studio_operation_type_name == 'Return to Vendor' and picking.x_studio_still_in_approval == True:
                 picking.show_validate = False

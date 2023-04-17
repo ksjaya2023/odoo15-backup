2d35f09d105c5584b943be095b3646f0e245283d
probo <sukmohadi.probo@gmail.com>
2023-04-17 09:15:36 +0700

    [UPD] overide destination journal for advance payment

--- hit_hybrid_customization/models/account_payment.py
+++ hit_hybrid_customization/models/account_payment.py
@@ -101,22 +101,61 @@ class AccountPayment(models.Model):
     #         lambda pay: pay.is_internal_transfer and not pay.paired_internal_transfer_payment_id
     #     )._create_paired_internal_transfer_payment()
 
-    @api.depends('journal_id', 'payment_type', 'payment_method_line_id','advance')
-    def _compute_outstanding_account_id(self):
+    # @api.depends('journal_id', 'payment_type', 'payment_method_line_id','advance')
+    # def _compute_outstanding_account_id(self):
+    #     for pay in self:
+    #         if pay.advance:
+    #             if pay.payment_type == 'inbound':
+    #                 pay.outstanding_account_id = pay.partner_id.x_studio_advance_sales_id
+    #             elif pay.payment_type == 'outbound':
+    #                 pay.outstanding_account_id = pay.partner_id.x_studio_advance_purchase_id
+    #             else:
+    #                 pay.outstanding_account_id = False
+    #         else
+    #             if pay.payment_type == 'inbound':
+    #                 pay.outstanding_account_id = (pay.payment_method_line_id.payment_account_id
+    #                                               or pay.journal_id.company_id.account_journal_payment_debit_account_id)
+    #             elif pay.payment_type == 'outbound':
+    #                 pay.outstanding_account_id = (pay.payment_method_line_id.payment_account_id
+    #                                               or pay.journal_id.company_id.account_journal_payment_credit_account_id)
+    #             else:
+    #                 pay.outstanding_account_id = False
+
+    @api.depends('journal_id', 'partner_id', 'partner_type', 'is_internal_transfer','advance')
+    def _compute_destination_account_id(self):
+        self.destination_account_id = False
         for pay in self:
-            if pay.advance:
-                if pay.payment_type == 'inbound':
-                    pay.outstanding_account_id = pay.partner_id.x_studio_advance_sales_id
-                elif pay.payment_type == 'outbound':
-                    pay.outstanding_account_id = pay.partner_id.x_studio_advance_purchase_id
+            if pay.is_internal_transfer:
+                pay.destination_account_id = pay.journal_id.company_id.transfer_account_id
+            elif pay.partner_type == 'customer':
+                # Receive money from invoice or send money to refund it.
+                if pay.partner_id:
+                    if pay.advance:
+                        if pay.payment_type == 'inbound':
+                            pay.destination_account_id = pay.partner_id.x_studio_advance_sales_id
+                        else:
+                            pay.destination_account_id = pay.partner_id.x_studio_advance_purchase_id
+                    else:
+                        pay.destination_account_id = pay.partner_id.with_company(pay.company_id).property_account_receivable_id
                 else:
-                    pay.outstanding_account_id = False
-            else
-                if pay.payment_type == 'inbound':
-                    pay.outstanding_account_id = (pay.payment_method_line_id.payment_account_id
-                                                  or pay.journal_id.company_id.account_journal_payment_debit_account_id)
-                elif pay.payment_type == 'outbound':
-                    pay.outstanding_account_id = (pay.payment_method_line_id.payment_account_id
-                                                  or pay.journal_id.company_id.account_journal_payment_credit_account_id)
+                    pay.destination_account_id = self.env['account.account'].search([
+                        ('company_id', '=', pay.company_id.id),
+                        ('internal_type', '=', 'receivable'),
+                        ('deprecated', '=', False),
+                    ], limit=1)
+            elif pay.partner_type == 'supplier':
+                # Send money to pay a bill or receive money to refund it.
+                if pay.partner_id:
+                    if pay.advance:
+                        if pay.payment_type == 'inbound':
+                            pay.destination_account_id = pay.partner_id.x_studio_advance_sales_id
+                        else:
+                            pay.destination_account_id = pay.partner_id.x_studio_advance_purchase_id
+                    else:
+                        pay.destination_account_id = pay.partner_id.with_company(pay.company_id).property_account_payable_id
                 else:
-                    pay.outstanding_account_id = False
+                    pay.destination_account_id = self.env['account.account'].search([
+                        ('company_id', '=', pay.company_id.id),
+                        ('internal_type', '=', 'payable'),
+                        ('deprecated', '=', False),
+                    ], limit=1)

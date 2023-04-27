adf1140a5a0625de8549d45a61a7b97a6a6c3a2b
probo <sukmohadi.probo@gmail.com>
2023-04-27 09:24:58 +0700

    [UPD] causing error on update stock move

--- hit_studio_prevent_error/models/stock_move.py
+++ hit_studio_prevent_error/models/stock_move.py
@@ -14,13 +14,13 @@ class StockMove(models.Model):
             del vals['product_qty']
         return super(StockMove, self).create(vals)
 
-    @api.model
-    def write(self, vals):
-        # _logger.error('###############')
-        # _logger.error(vals)
-        if 'product_qty' in vals:
-            del vals['product_qty']
-        return super(StockMove, self).write(vals)
+    # @api.model
+    # def write(self, vals):
+    #     # _logger.error('###############')
+    #     # _logger.error(vals)
+    #     if 'product_qty' in vals:
+    #         del vals['product_qty']
+    #     return super(StockMove, self).write(vals)
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
